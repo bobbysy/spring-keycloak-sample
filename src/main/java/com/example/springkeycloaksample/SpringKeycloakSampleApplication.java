41d4eb3b9b23892ce9e1241d8f85275f74ff7fa6
bobbysy <>
2020-07-27 17:30:38 +0800

    feat: springboot with keycloak integration

---
+++ src/main/java/com/example/springkeycloaksample/SpringKeycloakSampleApplication.java
@@ -0,0 +1,12 @@
+package com.example.springkeycloaksample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKeycloakSampleApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringKeycloakSampleApplication.class, args);
+  }
+}
---
+++ src/test/java/com/example/springkeycloaksample/SpringKeycloakSampleApplicationTests.java
@@ -0,0 +1,11 @@
+package com.example.springkeycloaksample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKeycloakSampleApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
